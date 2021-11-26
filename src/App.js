e80f96de3762472f57091aacf3d24a5dfc7e5d98
Ruben Tiago Silva <rubentiago3@gmail.com>
2021-11-26 01:40:02 +0000

    logic to add items to the todo list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 const App = () => {
   const [text , setText] = useState('');
   const [list , setList] = useState([]);
+  const [hasItens, setHasItens] = useState(false);
+  
+  useEffect(()=> {
+    if(list.length > 0) {
+      setHasItens(true);
+    }else{
+      setHasItens(false);
+    }
+  }, [list])
  
   return (
     <div className="wrapper">
@@ -15,18 +24,72 @@ const App = () => {
         <div className="center">
          <form>
             <label for="add-task">Add task</label>
-            <input type="text" id="add-task" name="add-task" value={text} onChange={(e)=> setText(e.target.value)}/>
+            <input type="Text" id="add-task" name="add-task" value={text} onChange={(e)=> setText(e.target.value)}/>
               <button onClick={(e) => { 
                 e.preventDefault();
-                setList([...list, text]);
+                if(text===''){ 
+                  return;
+                }
+                
+                const newItem = {
+                  value:text,
+                  checked:false,
+                };
+                setList([...list,newItem]);
                 setText('');
               }}>
               Submit
               </button>
+              <button onClick={(e) => {
+                e.preventDefault();
+                setList([]);          
+              }}>Clear all</button>
+
+               { hasItens && <>
+                  <button onClick={(e) => {
+                    e.preventDefault();
+                    setList(list.map((elem,i) =>{          
+                              return {
+                                value: elem.value,
+                                checked: true
+                              }    
+                        })
+                    )}}>Check All</button>
+
+                  <button onClick={(e) => {
+                    e.preventDefault();
+                    setList(list.map((elem,i) =>{          
+                              return {
+                                value: elem.value,
+                                checked: false
+                              }    
+                        })
+                    )}}>Uncheck All</button>    
+                  </>     
+                }
          </form> 
+
          <ul>
-            {list.map((elem) =>
-               <li>{elem}</li>
+            {list.map((elem,index) =>
+              <li>
+                <input checked={elem.checked} 
+                    onChange={()=>{
+                      setList(list.map((elem,i) =>{
+                          if(i===index){
+                            return {
+                              value: elem.value,
+                              checked:!elem.checked
+                            }
+                          }
+                          return elem;
+                      }))
+                    }}
+                    type="checkbox" />
+                {elem.value}
+                <button onClick={(e) => {
+                    setList(list.filter((elem,i) => i!==index))   
+                }}>X</button>
+              </li>
            )}
          </ul>
         </div>
